8bb7f7d13617f645c55ecf8a86e8c62d5e5001ab
Royal Finch <Royal2791@gmail.com>
2024-02-20 22:42:02 -0500

    Add location heading.

--- src/index.js
+++ src/index.js
@@ -3,23 +3,33 @@ import './css/style.css';
 
 const FORECAST_LENGTH = 3;
 const API_KEY = '245f101ed3434e6f98903140242102';
-const LOCATION = 'massillon';
 
 const container = document.querySelector('.container');
 const form = document.querySelector('form');
 
-async function getForecast(location) {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${FORECAST_LENGTH}&aqi=no&alerts=no`,
-  );
-  const data = await response.json();
-  const forecast = data.forecast.forecastday;
+function createForecastContent(forecast) {
   forecast.forEach((day) => {
     const div = document.createElement('div');
     div.classList.add('forecast');
     div.textContent = day.day.condition.text;
     container.appendChild(div);
   });
+}
+
+function createLocationHeading(location) {
+  const header = document.querySelector('#locationHeading');
+  header.textContent = `${location.name}, ${location.region}`;
+}
+
+async function getForecast(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${FORECAST_LENGTH}&aqi=no&alerts=no`,
+  );
+  const data = await response.json();
+  const locationData = data.location;
+  const forecastData = data.forecast.forecastday;
+  createLocationHeading(locationData);
+  createForecastContent(forecastData);
 }
 
 form.addEventListener('submit', (event) => {
@@ -29,3 +39,6 @@ form.addEventListener('submit', (event) => {
   container.textContent = '';
   getForecast(data.location);
 });
+
+// TODO
+// handle error if location not found
