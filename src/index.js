9490a4639781ff60b987b31fa4de2e08812c20f9
Royal Finch <Royal2791@gmail.com>
2024-02-20 22:13:46 -0500

    set up project and add basic features
    Allow user to enter location.
    Display the 3 day forcast upon submit.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import './css/reset.css';
 import './css/style.css';
+
+const FORECAST_LENGTH = 3;
+const API_KEY = '245f101ed3434e6f98903140242102';
+const LOCATION = 'massillon';
+
+const container = document.querySelector('.container');
+const form = document.querySelector('form');
+
+async function getForecast(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${FORECAST_LENGTH}&aqi=no&alerts=no`,
+  );
+  const data = await response.json();
+  const forecast = data.forecast.forecastday;
+  forecast.forEach((day) => {
+    const div = document.createElement('div');
+    div.classList.add('forecast');
+    div.textContent = day.day.condition.text;
+    container.appendChild(div);
+  });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  container.textContent = '';
+  getForecast(data.location);
+});
