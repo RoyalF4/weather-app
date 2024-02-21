ca8192e0c769d78f5d394a5319393cfa38dc7832
Royal Finch <Royal2791@gmail.com>
2024-02-21 16:44:06 -0500

    add form validation for user input

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ const API_KEY = '245f101ed3434e6f98903140242102';
 
 const container = document.querySelector('.container');
 const form = document.querySelector('form');
+const locationInput = document.querySelector('#location');
+const inputError = document.querySelector('span.error');
+const locationHeading = document.querySelector('#locationHeading');
+
+function showError() {
+  if (locationInput.validity.valueMissing) {
+    inputError.textContent = 'Enter the name of a city.';
+  } else if (!locationInput.validity.valid) {
+    inputError.textContent = 'Input can only contain letters.';
+  } else {
+    inputError.textContent = 'Not a valid city name.';
+  }
+  inputError.className = 'error active';
+}
+
+locationInput.addEventListener('input', () => {
+  if (locationInput.validity.valid) {
+    inputError.textContent = '';
+    inputError.className = 'error';
+  } else {
+    showError();
+  }
+});
 
 function createForecastContent(forecast) {
   forecast.forEach((day) => {
@@ -16,29 +39,49 @@ function createForecastContent(forecast) {
   });
 }
 
-function createLocationHeading(location) {
-  const header = document.querySelector('#locationHeading');
-  header.textContent = `${location.name}, ${location.region}`;
+function setLocationText(location) {
+  if (location === undefined) {
+    locationHeading.textContent = '';
+  } else {
+    locationHeading.textContent = `${location.name}, ${location.region}`;
+  }
 }
 
 async function getForecast(location) {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${FORECAST_LENGTH}&aqi=no&alerts=no`,
-  );
-  const data = await response.json();
-  const locationData = data.location;
-  const forecastData = data.forecast.forecastday;
-  createLocationHeading(locationData);
-  createForecastContent(forecastData);
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=${FORECAST_LENGTH}&aqi=no&alerts=no`,
+    );
+    if (response.status === 400) {
+      showError();
+      throw new Error('Bad Request!');
+    }
+    const data = await response.json();
+    const locationData = data.location;
+    const forecastData = data.forecast.forecastday;
+    setLocationText(locationData);
+    createForecastContent(forecastData);
+  } catch (error) {
+    setLocationText();
+  }
 }
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const formData = new FormData(form);
+//   const data = Object.fromEntries(formData);
+//   container.textContent = '';
+//   getForecast(data.location);
+// });
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-  container.textContent = '';
-  getForecast(data.location);
+  if (!locationInput.validity.valid) {
+    showError();
+  } else {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    container.textContent = '';
+    getForecast(data.location);
+  }
 });
-
-// TODO
-// handle error if location not found
